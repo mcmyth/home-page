58acdf184b8884b7425020dc2943973dc26068b5
MC_Myth <1440126177@qq.com>
2021-03-22 22:11:56 +0800

    :sparkles: improve drawer ui

--- src/components/drawers.vue
+++ src/components/drawers.vue
@@ -1,7 +1,8 @@
 <template>
 <div class="drawers">
-  <div class="button" @click="show = !show">M</div>
-  <div v-show="show" class="context">
+  <div class="button" @click="show = !show">D</div>
+  <div :style="show ? 'left: 0' : 'left: -250px'" class="context">
+    <span class="title">Drawer</span>
     <ul>
       <li>item</li>
       <li>item</li>
@@ -10,6 +11,7 @@
       <li>item</li>
     </ul>
   </div>
+  <div class="mask" :style="show ? 'opacity: 1;' : 'opacity: 0;pointer-events: none;'" @click="show = !show"></div>
 </div>
 </template>
 
@@ -41,21 +43,51 @@ export default {
   left: 0
   top: 0
   height: 100vh
-  background-color: red
+  background-color: white
   box-sizing: border-box
   font-size: 1rem
-  min-width: 200px
-  max-width: 100vw
+  width: 250px
   line-height: 1rem
+  z-index: 10
+  transition: left .2s ease-in-out
+  .title
+    color: #4f4f4f
+    font-size: 1.4rem
+    text-align: left
+    margin: 15px 0 25px 25px
+    display: block
+    position: relative
+    &::before
+      content: ""
+      display: block
+      height: 1px
+      width: 230px
+      background-color: #dfdfdf
+      position: absolute
+      left: -15px
+      bottom: -20px
   ul
     list-style: none
     padding: 0
     display: flex
     flex-direction: column
+    color: #333
     li
       height: auto
-      background-color: green
-      padding: 10px
+      transition: background-color .3s ease-in-out
+      user-select: none
+      cursor: pointer
+      &:hover
+        background-color: rgba(black, .1)
+      padding: 15px 0 15px 25px
       margin: 10px 0
       box-sizing: border-box
+      text-align: left
+.mask
+  position: fixed
+  z-index: 1
+  width: 100vw
+  height: 100vh
+  background-color: rgba(black, .3)
+  transition: opacity .2s linear
 </style>
